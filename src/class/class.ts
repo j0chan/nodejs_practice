c4d004f2b8970bcdd8700a8c19269726a915333b
Youngchan <wdc4512@naver.com>
2025-01-09 15:50:59 +0900

    클래스 사용 양식

--- src/class/class.ts
+++ src/class/class.ts
@@ -47,3 +47,31 @@ class Pet{
 let p1 = new Pet();
 console.log(p1.category);
 console.log(p1.name);
+
+
+class User {
+    // Field
+    username: string;
+    email: string;
+    job: string = "Student"; // 기본값 지정
+
+    // Constructor
+    constructor(username:string, email:string){
+        this.username = username;
+        this.email = email;
+    };
+
+    // Method
+    study(): void {
+        console.log(`${this.username} is studying.`);
+    }
+}
+
+// Create Object
+let user1 = new User("홍길동", "hong@abcd.co.kr")
+let user2 = new User("김철수", "kim@abcd.co.kr")
+
+console.log(user1)
+console.log(user2.email)
+user1.study();
+user2.study();
